2db970477318984d182f2dd415d648ede765b86f
DarkYusuf13 <yusufdegermenci61616161@gmail.com>
2017-12-07 22:01:45 +0300

    upload

---
+++ src/pocketmine/level/generator/Void.php
@@ -0,0 +1,111 @@
+<?php
+
+#______           _    _____           _                  
+#|  _  \         | |  /  ___|         | |                 
+#| | | |__ _ _ __| | _\ `--. _   _ ___| |_ ___ _ __ ___   
+#| | | / _` | '__| |/ /`--. \ | | / __| __/ _ \ '_ ` _ \  
+#| |/ / (_| | |  |   </\__/ / |_| \__ \ ||  __/ | | | | | 
+#|___/ \__,_|_|  |_|\_\____/ \__, |___/\__\___|_| |_| |_| 
+#                             __/ |                       
+#                            |___/
+
+namespace pocketmine\level\generator;
+
+use pocketmine\block\Block;
+use pocketmine\level\ChunkManager;
+use pocketmine\level\format\Chunk;
+use pocketmine\math\Vector3;
+use pocketmine\utils\Random;
+
+class Void extends Generator{
+
+	/** @var ChunkManager */
+	private $level;
+	/** @var Chunk */
+	private $chunk;
+	/** @var Random */
+	private $random;
+	private $options;
+	/** @var Chunk */
+	private $emptyChunk = null;
+
+	/**
+	 * @return array
+	 */
+	public function getSettings(){
+		return [];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName(){
+		return "Void";
+	}
+
+	/**
+	 * @param array $settings
+	 */
+	public function __construct(array $settings = []){
+		$this->options = $settings;
+	}
+
+	/**
+	 * @param ChunkManager $level
+	 * @param Random       $random
+	 *
+	 * @return mixed|void
+	 */
+	public function init(ChunkManager $level, Random $random){
+		$this->level = $level;
+		$this->random = $random;
+	}
+
+	/**
+	 * @param $chunkX
+	 * @param $chunkZ
+	 *
+	 * @return mixed|void
+	 */
+	public function generateChunk($chunkX, $chunkZ){
+		if($this->emptyChunk === null){
+			$this->chunk = clone $this->level->getChunk($chunkX, $chunkZ);
+			$this->chunk->setGenerated();
+
+			for($Z = 0; $Z < 16; ++$Z){
+				for($X = 0; $X < 16; ++$X){
+					$this->chunk->setBiomeId($X, $Z, 1);
+					for($y = 0; $y < 128; ++$y){
+						$this->chunk->setBlockId($X, $y, $Z, Block::AIR);
+					}
+				}
+			}
+
+			$spawn = $this->getSpawn();
+			if($spawn->getX() >> 4 === $chunkX and $spawn->getZ() >> 4 === $chunkZ){
+				$this->chunk->setBlockId(0, 64, 0, Block::GRASS);
+			}else{
+				$this->emptyChunk = clone $this->chunk;
+			}
+		}else{
+			$this->chunk = clone $this->emptyChunk;
+		}
+
+		$chunk = clone $this->chunk;
+		$chunk->setX($chunkX);
+		$chunk->setZ($chunkZ);
+		$this->level->setChunk($chunkX, $chunkZ, $chunk);
+	}
+
+    public function populateChunk($chunkX, $chunkZ){
+    	
+	}
+
+	/**
+	 * @return Vector3
+	 */
+	public function getSpawn(){
+		return new Vector3(0, 68, 0);
+	}
+	
+}
