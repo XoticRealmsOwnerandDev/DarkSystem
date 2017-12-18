4f5eeb129256584b7e5a5f9f993b14fe54a52637
- G2P - <guyza01.2547@gmail.com>
2017-12-18 15:42:31 +0700

    Fix 99

--- src/pocketmine/level/ChunkManager.php
+++ src/pocketmine/level/ChunkManager.php
@@ -11,18 +11,58 @@
 
 namespace pocketmine\level;
 
+use pocketmine\level\format\Chunk;
+
 interface ChunkManager{
-	
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     * @return mixed
+     */
 	public function getBlockIdAt($x, $y, $z);
-	
+
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     * @param $id
+     * @return mixed
+     */
 	public function setBlockIdAt($x, $y, $z, $id);
-	
+
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     * @return mixed
+     */
 	public function getBlockDataAt($x, $y, $z);
-	
+
+    /**
+     * @param $x
+     * @param $y
+     * @param $z
+     * @param $data
+     * @return mixed
+     */
 	public function setBlockDataAt($x, $y, $z, $data);
-	
+
+    /**
+     * @return mixed
+     */
 	public function getYMask();
-	
+
+    /**
+     * @return mixed
+     */
 	public function getMaxY();
-	
+
+    /**
+     * @param int $chunkX
+     * @param int $chunkZ
+     *
+     * @return Chunk|null
+     */
+    public function getChunk(int $chunkX, int $chunkZ);
 }
